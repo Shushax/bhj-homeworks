1dcb2130e32352c2e4ea3b40ebf81366a0877f96
Shushax <nekfi@mail.ru>
2020-07-25 00:48:13 +0300

    Окончательные правки

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -4,7 +4,7 @@ let lost = document.getElementById('lost');
 
 for (i = 1; i <= 9; i++) {
     getHole(i).onclick = function() {
-        if (getHole(i).className.includes('hole__has-mole')) {
+        if (this.className.includes('hole_has-mole')) {
             dead.textContent = Number(dead.textContent) + 1;
         } else {
             lost.textContent = Number(lost.textContent) + 1;
