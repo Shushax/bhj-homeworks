afe881befb97390ede755530228fac905ae55be9
Shushax <nekfi@mail.ru>
2020-07-25 19:32:06 +0300

    Правки

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,6 +1,5 @@
 let menuLink = document.getElementsByClassName('menu__link');
 let links = Array.from(menuLink);
-let menu_sub = document.getElementsByClassName('menu_sub')
 for (let link of links) {
     link.onclick = function() {
         if (link.nextElementSibling) {
