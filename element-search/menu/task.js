4c7aeb050ea03daa7e8bf7e89d111c96eaa13080
Shushax <nekfi@mail.ru>
2020-07-26 18:52:00 +0300

    Правки 1 и 2 задание

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,8 +2,11 @@ let menuLink = document.getElementsByClassName('menu__link');
 let links = Array.from(menuLink);
 for (let link of links) {
     link.onclick = function() {
-        if (link.nextElementSibling) {
-            link.nextElementSibling.className = 'menu menu_sub menu_active';
+        if (link.nextElementSibling.className.includes('menu_active')) {
+            link.nextElementSibling.classList.remove('menu_active');
+            return false;
+        } else if (link.nextElementSibling) {
+            link.nextElementSibling.classList.add('menu_active');
             return false;
         }
     }
