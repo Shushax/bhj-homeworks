266085555d4e087aa5485a415efeead8b06003f4
Shushax <nekfi@mail.ru>
2020-08-10 00:22:01 +0300

    Правки 2 задание

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -6,24 +6,22 @@ input.onkeydown = function(e) {
     if (enter === 'Enter' && (input.value === '' || input.value === ' ')) {
         return false;
     } else if (enter === 'Enter') {
-        let todo = document.createElement('div');
-        todo.className = 'task';
-        todo.innerHTML = `<div class="task__title">${input.value}</div><a href="#" class="task__remove">&times;</a>`;
-        tasks__list.insertAdjacentElement('beforeend', todo);
-        document.forms[0].reset();
-        let elementRemove = todo.lastChild;
-        elementRemove.onclick = function() {
-            todo.remove();
-        }
+        toDo();
         return false;
     }    
 }
 
-button.onclick = function() {
+button.onclick = function(e) {
+    e.preventDefault();
     if (input.value === '' || input.value === ' ') {
         return false;
     } else {
-        let todo = document.createElement('div');
+        toDo();
+    }  
+}
+
+function toDo() {
+    let todo = document.createElement('div');
         todo.className = 'task';
         todo.innerHTML = `<div class="task__title">${input.value}</div><a href="#" class="task__remove">&times;</a>`;
         tasks__list.insertAdjacentElement('beforeend', todo);
@@ -32,5 +30,4 @@ button.onclick = function() {
         elementRemove.onclick = function() {
             todo.remove();
         }  
-    }
 }
